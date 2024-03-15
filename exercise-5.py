596b88a2998e18d9601e3b31461993cc84c94e4f
Nikola-Limpet <156181712+Nikola-Limpet@users.noreply.github.com>
2024-03-15 22:43:01 +0700

    Update exercise-5.py
    Done

--- exercise-5.py
+++ exercise-5.py
@@ -10,14 +10,22 @@ def reverse_ascending(numbers):
             temp_subseq.append(numbers[i])  # If so, it's part of an ascending subsequence
         else:
             # If not, the current ascending subsequence end here, so reverse and append it to the result
+            
             result.extend(temp_subseq[::-1])
             temp_subseq = [numbers[i]]  # Start a new subsequence with the current number
     
     # add the last subsequence after exiting the loop
     result.extend(temp_subseq[::-1])
-    return result
     
-print(reverse_ascending([1, 2, 3, 4, 5]))  
-print(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3]))  
-print(reverse_ascending([5, 4, 3, 2, 1]))  
-print(reverse_ascending([])) 
+    return result
+
+# ask user to enter number by separate by space
+user_input = input("Enter a list of numbers separated by space: ")
+nums = user_input.split() # split each number
+for num in  range(len(nums)):
+
+    nums[num] = int(nums[num])
+
+     #Convert the input string to a list of integers
+
+print(reverse_ascending(nums))
